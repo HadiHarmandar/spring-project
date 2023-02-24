b8bd19deb9b411f00078fc5e95486aa89e92459c
HadiHarmandar <hadiharmandar1@gmail.com>
2023-02-24 16:54:16 -0600

    Autowiring

--- spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/PC.java
+++ spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/PC.java
@@ -3,15 +3,20 @@ package com.cydeo.streotype_annotation;
 import com.cydeo.streotype_annotation.casefactory.Case;
 import com.cydeo.streotype_annotation.monitorfactory.Monitor;
 import com.cydeo.streotype_annotation.motherboardfactory.Motherboard;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Getter
+@Component
 public class PC {
 
     private Case theCase;
     private Monitor monitor;
     private Motherboard motherboard;
 
+    // @Autowired happens behind the scene, because we have only one constructor!!!
     public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
         this.theCase = theCase;
         this.monitor = monitor;
