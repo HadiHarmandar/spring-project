dbcac449fd808bb5baf9a956f53e3ca23f7729d8
HadiHarmandar <hadiharmandar1@gmail.com>
2023-03-03 17:15:34 -0600

    Thymeleaf

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -19,8 +19,8 @@ public class StudentController {
     }
 
     @RequestMapping("/welcome") // localhost:8080/student/welcome?name=Ozzy
-    public String drop(@RequestParam String name) {
-        System.out.println(name);
+    public String drop() {  //@RequestParam String name
+        //System.out.println(name);
         return "student/welcome";
     }
 
