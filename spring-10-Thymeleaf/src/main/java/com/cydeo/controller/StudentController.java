fca348e526298392bb5a9f74bca4b52ab4465572
HadiHarmandar <hadiharmandar1@gmail.com>
2023-03-07 10:12:07 -0600

    Thymeleaf

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,8 +20,14 @@ public class StudentController {
     }
 
     @RequestMapping("/welcome") // localhost:8080/student/welcome?name=Ozzy
-    public String drop() {  //@RequestParam String name
-        //System.out.println(name);
+    public String drop(@RequestParam String name) {
+        System.out.println(name);
+        return "student/welcome";
+    }
+
+    @RequestMapping("/welcome/{name") // localhost:8080/student/welcome/Ozzy
+    public String welcome(@PathVariable String name) {
+
         return "student/welcome";
     }
 
