03ea7e023415514f3a32ef2f0165efd1b9ec0f9a
HadiHarmandar <hadiharmandar1@gmail.com>
2023-03-10 00:05:54 -0600

    Thymeleaf

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -3,15 +3,14 @@ package com.cydeo.controller;
 import com.cydeo.bootstrap.DataGenerator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
 
-    @RequestMapping("/register") // localhost:8080/student/register
+//    @RequestMapping(value = "/register", method = RequestMethod.GET) // localhost:8080/student/register
+    @GetMapping("/register")
     public String register(Model model) {
 
         model.addAttribute("students", DataGenerator.createStudent());
@@ -19,13 +18,14 @@ public class StudentController {
         return "student/register";
     }
 
-    @RequestMapping("/welcome") // localhost:8080/student/welcome?name=Ozzy
+    @RequestMapping(value = "/welcome", method = RequestMethod.POST) // localhost:8080/student/welcome?name=Ozzy
+    @PostMapping("/welcome")
     public String drop(@RequestParam String name) {
         System.out.println(name);
         return "student/welcome";
     }
 
-    @RequestMapping("/welcome/{name") // localhost:8080/student/welcome/Ozzy
+    @RequestMapping("/welcome/{name}") // localhost:8080/student/welcome/Ozzy
     public String welcome(@PathVariable String name) {
 
         return "student/welcome";
