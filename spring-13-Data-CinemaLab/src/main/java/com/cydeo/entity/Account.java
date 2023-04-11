eeeb9ab887baaa8431a4767fcc97e535af743ea2
HadiHarmandar <hadiharmandar1@gmail.com>
2023-04-11 15:05:12 -0500

    Cinema Lab

---
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Account.java
@@ -0,0 +1,30 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.UserRole;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "account_details")
+public class Account extends BaseEntity {
+
+    private String name;
+    private String address;
+    private String country;
+    private String state;
+    private String city;
+    private Integer age;
+    private String postalCode;
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @OneToOne(mappedBy = "account")
+    private User user;
+
+}
