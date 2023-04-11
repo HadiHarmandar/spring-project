efe48379a32398dae55d55eb691ca947a6528f6c
HadiHarmandar <hadiharmandar1@gmail.com>
2023-04-11 13:53:00 -0500

    Cinema Lab

---
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Location.java
@@ -0,0 +1,24 @@
+package com.cydeo.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Location extends BaseEntity {
+
+    private String name;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+    private String postalCode;
+    private String country;
+    private String state;
+    private String city;
+    private String address;
+}
