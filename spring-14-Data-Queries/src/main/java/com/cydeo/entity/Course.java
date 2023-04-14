29c197a0e28401120af8243a202570b68fdc4d85
HadiHarmandar <hadiharmandar1@gmail.com>
2023-04-14 11:55:54 -0500

    Spring DATA Queries

---
+++ spring-14-Data-Queries/src/main/java/com/cydeo/entity/Course.java
@@ -0,0 +1,28 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Table(name = "COURSES")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "NAME")
+    private String name;
+    @Column(name = "CATEGORY")
+    private String category;
+    @Column(name = "RATING")
+    private Integer rating;
+    @Column(name = "DESCRIPTION")
+    private String description;
+}
