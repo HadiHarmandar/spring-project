b3d0a094132becc45ee68d07e5cd0cc381f3e759
HadiHarmandar <hadiharmandar1@gmail.com>
2023-05-28 09:48:09 -0500

    Spring REST - Jackson Annotations

---
+++ spring-16-REST-Jackson/src/main/java/com/cydeo/dto/UserDTO.java
@@ -0,0 +1,25 @@
+package com.cydeo.dto;
+
+import com.cydeo.enums.UserRole;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    @JsonIgnore
+    private String email;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) // deserialisation ok, serialisation no
+    private String password;
+    private String username;
+    private UserRole role;
+
+    @JsonManagedReference // this field will be serialized
+    private AccountDTO account;
+
+}
