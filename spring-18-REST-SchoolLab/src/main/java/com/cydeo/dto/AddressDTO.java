6fb63f753d10397833f2dcd59d6b6eab62d6c965
HadiHarmandar <hadiharmandar1@gmail.com>
2023-05-28 10:53:12 -0500

    Spring REST - School Lab

--- spring-18-REST-SchoolLab/src/main/java/com/cydeo/dto/AddressDTO.java
+++ spring-18-REST-SchoolLab/src/main/java/com/cydeo/dto/AddressDTO.java
@@ -1,6 +1,10 @@
 package com.cydeo.dto;
 
 import com.cydeo.enums.AddressType;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +14,11 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class AddressDTO {
 
+    @JsonIgnore
     private Long id;
 
     private String street;
@@ -22,10 +29,13 @@ public class AddressDTO {
 
     private AddressType addressType;
 
+    @JsonBackReference(value = "student-address-reference")
     private StudentDTO student;
 
+    @JsonBackReference(value = "parent-address-reference")
     private ParentDTO parent;
 
+    @JsonBackReference(value = "teacher-address-reference")
     private TeacherDTO teacher;
 
     private Integer currentTemperature;
